3a5fa656096b370f512c900200f50283e562d083
koffi <jeankoffi543@gmal.com>
2025-11-17 13:24:51 +0000

    php artisan vendor:publish --tag=parametermap

--- src/Middleware/MapRequestParameters.php
+++ src/Middleware/MapRequestParameters.php
@@ -8,11 +8,34 @@ use Kjos\ParameterMapper\Support\ParameterMapper;
 
 class MapRequestParameters
 {
+    protected array $internalKeys;
+
+    public function __construct()
+    {
+        // Récupère toutes les valeurs backend de ta map
+        $this->internalKeys = array_values(config('parameter-mapper.map', []));
+    }
+
     /**
      * Handle an incoming request.
      */
     public function handle(Request $request, Closure $next)
     {
+        $rejectKnows = config('parameter-mapper.map.reject_knowns', true);
+        if ($rejectKnows) {
+            foreach ($request->query() as $key => $value) {
+                if (in_array($key, $this->internalKeys, true)) {
+                    abort(404, "not found");
+                }
+            }
+
+            foreach ($request->post() as $key => $value) {
+                if (in_array($key, $this->internalKeys, true)) {
+                    abort(404, "not found");
+                }
+            }
+        }
+
         // GET parameters
         $query = ParameterMapper::apply($request->query());
         $request->query->replace($query);
